ab7719bf5337de239dac16c83e1114ad06c0d249
Eric Neunaber <eric@smips.com>
2012-03-11 22:35:36 -0500

    Fixed bug where it wouldn't work after paging

--- excludekeepers.user.js
+++ excludekeepers.user.js
@@ -2,7 +2,7 @@
 // @name           	Exclude Keepers From Yahoo Pre-Draft Values
 // @namespace      	com.yahoo.fantasy.baseball
 // @copyright      	2011, Eric Neunaber
-// @include        	http://baseball.fantasysports.yahoo.com/b1/*/prerank_auction_costs
+// @include        	http://baseball.fantasysports.yahoo.com/b1/*/prerank_auction_costs*
 // @require  		http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.7.1.min.js
 // ==/UserScript==
 (function() {
