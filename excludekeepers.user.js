87516026bb4977a58decaedaf11f6ef0d8b6c3f2
Eric Neunaber <eric@smips.com>
2012-03-11 21:17:44 -0500

    Initial creation of script

---
+++ excludekeepers.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name           	Exclude Keepers From Yahoo Pre-Draft Values
+// @namespace      	com.yahoo.fantasy.baseball
+// @copyright      	2011, Eric Neunaber
+// @include        	http://baseball.fantasysports.yahoo.com/b1/*/prerank_auction_costs
+// @require  		http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.7.1.min.js
+// ==/UserScript==
+(function() {
+
+
+	console.log("only here...");
+	
+
+})();
